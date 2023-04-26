4b7da3005517fd231e473c61c0711739da99b6d5
alimrrc <83086007+alimrrc@users.noreply.github.com>
2023-04-26 23:46:24 +0200

    Update Clustering.py
    Data transformation, final dataset not finalised yet

--- Clustering.py
+++ Clustering.py
@@ -6,8 +6,6 @@ Created on Fri Mar  3 16:49:44 2023
 @author: ali
 """
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import OneHotEncoder
-import numpy as np
 import pandas as pd
 
 # Load the dataset from a CSV file
@@ -18,39 +16,83 @@ dtypes = df.dtypes
 
 cols = df.columns
 
-# Get a list of columns that have object (string) data type
-string_cols = list(dtypes[dtypes == "object"].index)
-float_cols = list(dtypes[dtypes == "float"].index)
-
-other_cols = [col for col in cols if col not in string_cols and col not in float_cols]
-df.drop(columns=other_cols, inplace=True)
-
-replacement_dict = {col: 'Nothing' for col in string_cols}
-df.fillna(replacement_dict, inplace=True)
-
-replacement_dict = {col: 0 for col in float_cols}
-df.fillna(replacement_dict, inplace=True)
-
-for col in string_cols:
-    if "goods_code" in col:
+for col in cols:
+    if "goods_code" in col or "model" in col or "ID" in col or col == 'Nb_of_items':
         df.drop(col, axis=1, inplace=True)
-        string_cols = [column for column in string_cols if column != col]
-    else:
-        df[col] = df[col].apply(lambda x: str(x) if isinstance(x, int) else x)
 
-# Create a OneHotEncoder object
-encoder = OneHotEncoder()
+cols = df.columns
 
-# Fit the encoder to the categorical columns
-encoder.fit(df[string_cols])
+df.fillna('', inplace=True)
 
-# Transform the categorical columns into one-hot encoded features
-one_hot_encoded = encoder.transform(df[string_cols])
+unique_values = {}
+feature_cols = {}
+feature_df = {}
+features = ['item', 'price', 'make', 'purchas']
 
-# Replace the original categorical columns with the one-hot encoded features
-df.drop(columns=string_cols, inplace=True)
+for feature in features:
+    unique_values[feature] = []
+    feature_cols[feature] = []
+    
+    for col in cols:
+        if feature in col:
+            feature_cols[feature].append(col)
+    
+    feature_df[feature] = df[feature_cols[feature]]
+    
+    for i, row in feature_df[feature].iterrows():
+        
+        for val in row:
+            if feature == 'item' or feature == 'make':
+                raw_val = val.replace(" ", "")
+            else:
+                raw_val = val
+                
+            if raw_val not in unique_values[feature] and raw_val != '':
+                unique_values[feature].append(raw_val)            
 
-df = pd.concat([df, pd.DataFrame(one_hot_encoded.toarray(), columns=encoder.get_feature_names(string_cols))], axis=1)
+new_cols = []
+
+for feature in features:
+    if feature == 'item' or feature == 'make':       
+        for label_add_up in [' Number', ' Price']:
+            for val in unique_values[feature]:
+                new_cols.append(val + label_add_up)
+
+
+data = pd.DataFrame(columns = new_cols, index = df.index)
+
+count_item = {}
+count_make = {}
+price_item = {}
+price_make = {}
+
+for row in range(len(df.index)):
+    count_item[row] = {}
+    price_item[row] = {}
+    
+    for item in unique_values['item']:
+        count_item[row][item] = 0
+        price_item[row][item] = 0
+    
+    for col in range(len(feature_df['item'].columns)):
+        if feature_df['item'].iloc[row, col] != '':
+            count_item[row][item] += feature_df['purchas'].iloc[row, col]
+            price_item[row][item] += feature_df['price'].iloc[row, col]
+
+for row in range(len(df.index)):
+    count_make[row] = {}
+    price_make[row] = {}
+    
+    for make in unique_values['make']:
+        count_make[row][make] = 0
+        price_make[row][make] = 0
+    
+    for col in range(len(feature_df['make'].columns)):
+        if feature_df['make'].iloc[row, col] != '':
+            count_make[row][make] += feature_df['purchas'].iloc[row, col]
+            price_make[row][make] += feature_df['price'].iloc[row, col]
+            
+
 
 # Initialize the K-Means algorithm with 2 clusters
 kmeans = KMeans(n_clusters=2, random_state=0)
